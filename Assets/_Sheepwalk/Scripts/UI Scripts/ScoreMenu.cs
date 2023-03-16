3f2b96d4d60260b69cefefed65f5e175ef67b8ab
Biscuitbunny <60516813+Biscuitbunny@users.noreply.github.com>
2023-03-16 16:15:58 +0100

    UI Function Basics

---
+++ Assets/_Sheepwalk/Scripts/UI Scripts/ScoreMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMenu : MonoBehaviour
+{
+    public void RetryButton()
+    {
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("GameLevel");
+        print("StartGame");
+    }
+
+    public void QuitToMenu()
+     {
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("StartMenu");
+        print("Back to Menu");
+     }
+      
+}
